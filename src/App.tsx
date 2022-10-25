b6cb1577483b0e2d0c1d72f8fbeb8a8b46bcf425
gareth0712 <gareth_0712@hotmail.com>
2022-10-25 17:48:55 +0800

    Add routing pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from "react-router-dom";
+import "./App.css";
+// These are still regular React components
+// They serve specific route purpose as pages and thus can be saved in a separate pages directory
+// This is just a recommended practice, not required by React-router-dom library
+import Product from "./pages/Product";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Nav Bar</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/product">
+        <Product />
+      </Route>
     </div>
   );
 }
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,5 @@
+const Welcome = (): JSX.Element => {
+  return <h1>The welcome page</h1>;
+};
+
+export default Welcome;
